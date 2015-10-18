5b882eba3fe3d64647a59f1261c7143a1335aeac
ArthurYV <yangjingbing007@gmail.com>
2015-10-18 13:24:12 +0800

    初始化工程项目

---
+++ Timeline-RecordAnalysis/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Timeline-RecordAnalysis
+//
+//  Created by Arthury on 15/10/18.
+//  Copyright © 2015年 Arthury. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ Timeline-RecordAnalysisTests/Timeline_RecordAnalysisTests.m
@@ -0,0 +1,39 @@
+//
+//  Timeline_RecordAnalysisTests.m
+//  Timeline-RecordAnalysisTests
+//
+//  Created by Arthury on 15/10/18.
+//  Copyright © 2015年 Arthury. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Timeline_RecordAnalysisTests : XCTestCase
+
+@end
+
+@implementation Timeline_RecordAnalysisTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
